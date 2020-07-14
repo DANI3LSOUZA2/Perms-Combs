f3f974ae3fe5be32a9bcd68111ee0243548ffc34
DANI3LSOUZA2 <das@cesar.school>
2020-07-14 20:10:26 -0300

    Armazenamento de combinações em andamento

--- missao03.py
+++ missao03.py
@@ -1,31 +1,64 @@
 from itertools import product
+from simplejson_functions import *
 
 POSICOES = 4
 comb = 0
 old_list = []
+list_txt = {}
 
 
 def permutar(n):
+
     posicao_atual = POSICOES
     comb = 0
     permsTotal = []
 
     for x in range(POSICOES):
+
         comb = n ** posicao_atual + comb
         permsList = list(product(List, repeat=posicao_atual))
         posicao_atual -= 1
         permsTotal.append(permsList)
 
     for x in permsTotal:
+
         for i in x:
+
             if i in old_list:
                 continue
+
             else:
                 print(i)
-                old_list.append(i)
+                old_list.append(i)  # Old_list corresponde às combinações que não se repetem
 
     print("Total de combinações: {}".format(comb))
+
     return permsTotal
+
+
+# Função para mandar as combinações ao arquivo txt
+def converter_list_perms():
+
+    list_all = show_file('combinacoes.txt')
+
+    if list_all is None:
+        cont_comb = 1
+    else:
+        for y in list_all:
+            cont_comb = y['id']
+
+        print(cont_comb)
+
+    for comb in old_list:
+        x = "".join(comb)
+
+        list_txt = {
+            cont_comb: x
+        }
+
+        insert_file(list_txt, 'combinacoes.txt')
+
+        cont_comb += 1
 
 
 List_adc = 0
@@ -57,9 +90,6 @@ while True:
     else:
         break
 
-
 # comb = número de combinações
 
 # Mostrar cada combinação em forma de matriz no terminal
-
-
